d4d1f32626282dfd9cb9e929c95b34a5546de00a
mtezxx <mkukucko@gmail.com>
2024-03-25 16:45:54 +0100

    models

--- Domain/Models/Booking.cs
+++ Domain/Models/Booking.cs
@@ -1,6 +1,23 @@
+using System.Runtime.InteropServices.JavaScript;
+using Microsoft.VisualBasic;
+
 namespace Shared.Models;
 
 public class Booking
 {
+    public int Id { get; set; }
+    public DateOnly DateFrom { get; set; }
+    public DateOnly DateTo { get; set; }
+    public int BookedQuantity { get; set; }
+    public int ResourceId { get; set; }
     
+    public Booking(DateOnly dateFrom, DateOnly dateTo, int bookedQuantity, int resourceId)
+    {
+        DateFrom = dateFrom;
+        DateTo = dateTo;
+        BookedQuantity = bookedQuantity;
+        ResourceId = resourceId;
+    }
+    
+    public Booking(){}
 }
--- Domain/Models/Resource.cs
+++ Domain/Models/Resource.cs
@@ -2,5 +2,15 @@ namespace Shared.Models;
 
 public class Resource
 {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Quantity { get; set; }
+
+    public Resource(string name, int quantity)
+    {
+        Name = name;
+        Quantity = quantity;
+    }
     
+    public Resource(){}
 }
