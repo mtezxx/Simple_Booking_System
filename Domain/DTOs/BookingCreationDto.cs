0758b4f5014c28045d5f8c645b8c73b84a1a7110
mtezxx <mkukucko@gmail.com>
2024-03-26 23:11:20 +0100

    booking added

--- Domain/DTOs/BookingCreationDto.cs
+++ Domain/DTOs/BookingCreationDto.cs
@@ -2,11 +2,11 @@ namespace Shared.DTOs;
 
 public class BookingCreationDto
 {
-    public DateOnly DateFrom { get; }
-    public DateOnly DateTo { get; }
-    public int BookedQuantity { get; }
-    public int ResourceId { get; }
-    
+    public DateOnly DateFrom { get; init; }
+    public DateOnly DateTo { get; init; }
+    public int BookedQuantity { get; init; }
+    public int ResourceId { get; init; }
+
     public BookingCreationDto(DateOnly dateFrom, DateOnly dateTo, int bookedQuantity, int resourceId)
     {
         DateFrom = dateFrom;
